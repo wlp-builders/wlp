d72093344caac6af2afe87373483b5a6a7b5dcd0
Neil <maartenq@protonmail.com>
2025-01-15 12:56:00 +0100

    wlp-install protocol fix for when you are behind mod_proxy

--- wlp-install/index.php
+++ wlp-install/index.php
@@ -7,6 +7,16 @@ if (file_exists(__DIR__ . '/../wlp-config.php')) {
 
 require_once __DIR__.'/php-verifyInstallKey.php';
 
+// protocol fix for when you're behind mod_proxy, ex. using Podman container
+if (!empty($_SERVER['HTTP_X_FORWARDED_PROTO']) && $_SERVER['HTTP_X_FORWARDED_PROTO'] === 'https') {
+    // Set $_SERVER['HTTPS'] to 'on' to emulate HTTPS environment
+    $_SERVER['HTTPS'] = 'on';
+    
+    // Adjust other related variables as needed
+    $_SERVER['SERVER_PORT'] = 443; // Standard HTTPS port
+    $_SERVER['REQUEST_SCHEME'] = 'https'; // Define scheme if your app uses it
+}
+
 // generate placeholder suggestive names
 // Get the server name
 $serverName = $_SERVER['SERVER_NAME'] ?? 'domain.tld';
